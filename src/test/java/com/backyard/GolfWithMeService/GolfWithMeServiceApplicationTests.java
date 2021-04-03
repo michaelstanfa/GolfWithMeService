435b68cd279cc5479f5d91cc7eb012381eccb8ac
Michael Stanfa <stanfa.michael@gmail.com>
2021-04-03 10:28:00 -0500

    initial-commit

---
+++ src/test/java/com/backyard/GolfWithMeService/GolfWithMeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backyard.GolfWithMeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfWithMeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
